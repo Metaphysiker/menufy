7a7bee73ce1304ca2d35921c05946f5d58c2c051
Metaphysiker <s.raess@me.com>
2022-07-12 16:38:07 +0200

    working on menufy

--- public/class-menufy-public.php
+++ public/class-menufy-public.php
@@ -100,4 +100,58 @@ class Menufy_Public {
 
 	}
 
+	//add_action( 'init', 'menufy_add_shortcode' );
+
+	public function menufy_add_shortcode() {
+	    add_shortcode(
+				'menufy',
+				array( $this, 'menufy_func' )
+			);
+	}
+
+	public function menufy_func($atts){
+
+		$menu_items = wp_get_nav_menu_items("standard");
+		?>
+		<?php
+		print("<pre>".print_r($menu_items,true)."</pre>");
+		?>
+		<h1>Menufy</h1>
+		<?php
+			foreach ($menu_items as $menu_item) {
+				echo "<p>
+				{$menu_item->title}
+				</p>";
+			}
+		?>
+		<?php
+	}
+
+	function wp_get_menu_array($current_menu) {
+
+	    $array_menu = wp_get_nav_menu_items($current_menu);
+	    $menu = array();
+	    foreach ($array_menu as $m) {
+	        if (empty($m->menu_item_parent)) {
+	            $menu[$m->ID] = array();
+	            $menu[$m->ID]['ID']      =   $m->ID;
+	            $menu[$m->ID]['title']       =   $m->title;
+	            $menu[$m->ID]['url']         =   $m->url;
+	            $menu[$m->ID]['children']    =   array();
+	        }
+	    }
+	    $submenu = array();
+	    foreach ($array_menu as $m) {
+	        if ($m->menu_item_parent) {
+	            $submenu[$m->ID] = array();
+	            $submenu[$m->ID]['ID']       =   $m->ID;
+	            $submenu[$m->ID]['title']    =   $m->title;
+	            $submenu[$m->ID]['url']  =   $m->url;
+	            $menu[$m->menu_item_parent]['children'][$m->ID] = $submenu[$m->ID];
+	        }
+	    }
+	    return $menu;
+
+	}
+
 }
